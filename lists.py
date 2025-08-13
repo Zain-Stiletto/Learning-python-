9288753972cedd64666317ed9477e245970e485d
Zain-Stiletto <sheskwandisa@gmail.com>
2025-08-13 09:31:32 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,17 @@
+#lists
+
+fruits = ['apple', 'banana', 'cherry']
+
+print(fruits[0])
+
+fruits.append('kiwi')
+print(fruits)
+
+fruits.insert(1, 'orange')
+
+print(fruits)
+
+fruits.remove('kiwi')
+
+fruits.sort(reverse=True)
+print(fruits)
---
+++ shopping_cart.py
@@ -0,0 +1,28 @@
+#Shopping cart
+#create a shopping cart programme that will continously ask the user for a food product and the price of that product
+#have an exit clause if the user wishes to stop adding more thngs to their cart
+#At the end show the food items and the total cost to the user
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input('Enter a food to buy or press q to quit:  ')
+    if food.lower() == 'q':
+        break
+    else:
+        price = float(input(f'Enter the price of the {food}: R'))
+        foods.append(food)
+        prices.append(price)
+        
+print('------ YOUR CART -------')
+
+for food in foods:
+    print(food, end= ' ')
+    
+for price in prices:
+    total += price
+
+print('\n')  
+print(f'Your total is: R{total}')
